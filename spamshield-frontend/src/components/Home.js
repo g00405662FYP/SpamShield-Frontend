a0b29ab521690a23f900b22f4ed83161efc15401
CormacM22 <g00405662@atu.ie>
2024-11-18 11:14:50 +0000

    Add Home component and initialize Login and auth services

---
+++ spamshield-frontend/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to SpamShield</h1>
+      <p>Secure your communications from unwanted spam messages.</p>
+    </div>
+  );
+}
+
+export default Home;
